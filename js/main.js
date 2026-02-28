531d3bff7571236c4e22086241fe1ca697254ed5
ChrisBlues <chris.blues@free.fr>
2026-02-28 14:49:26 +0100

    Premier commit de SEO

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrolltop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+// Détection du bas de page pour les remerciements au concepteur
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY + window.innerHeight;
+  const pageHeight = document.documentElement.scrollHeight;
+  const footer = document.getElementById('thank-you-footer');
+
+  if (scrollPosition >= pageHeight - 50) {
+    footer.classList.add('visible');
+  } else {
+    footer.classList.remove('visible');
+  }
+});
